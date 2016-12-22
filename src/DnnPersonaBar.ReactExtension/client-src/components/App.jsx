94388e91276f66cb9dbd3bf197027c650d2bbc0c
Joe Brinkman <Joe@dnncorp.com>
2016-12-22 08:42:53 -0800

    Initial commit for the PersonaBar templates

---
+++ src/DnnPersonaBar.ReactExtension/client-src/components/App.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+import SocialPanelHeader from "dnn-social-panel-header";
+import PersonaBarPage from "dnn-persona-bar-page";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <PersonaBarPage isOpen="true">
+                    <SocialPanelHeader title="Extension">
+                    </SocialPanelHeader>
+                </PersonaBarPage>
+            </div>
+        );
+    }
+}
+
+
+
+
+export default (App);
